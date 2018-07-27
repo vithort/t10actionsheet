71766407f302317a244b12672e3b77fbb850b49a
vithort <vithort@gmail.com>
2018-07-27 15:03:56 -0300

    Update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public text: string;
 
+  constructor(
+    public navCtrl: NavController
+    , public actionSheetCtrl: ActionSheetController
+  ) {
+
+  }
+
+  presentActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Action Sheet Title',
+      buttons: [
+        {
+          text: 'Destructive',
+          icon: 'cancel',
+          role: 'destructive',
+          handler: () => {
+            this.text = 'Destructive clicked';
+          }
+        }, {
+          text: 'Archive',
+          icon: 'camera',
+          handler: () => {
+            this.text = 'Archive clicked';
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'home',
+          role: 'cancel',
+          handler: () => {
+            this.text = 'Cancel clicked';
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
